a020cc5d7b5581b2cd117d68f338567b90f69cf4
mahedy25 <mahedyhasan873@gmail.com>
2025-03-12 22:36:14 +0600

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME= process.env.NEXT_PUBLIC_APP_NAME || "Duwah"
+export const APP_SLOGAN= process.env.Next_PUBLIC_APP_SLOGAN || "Spend less, save more."
+export const APP_DESCRIPTION= process.env.Next_PUBLIC_APP_DESCRIPTION || "The best online store"
