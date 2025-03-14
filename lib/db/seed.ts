fd2a626610679331f1fb6af27c41817a5cb6a3bf
mahedy25 <mahedyhasan873@gmail.com>
2025-03-14 20:06:37 +0600

    mongoDB settings

---
+++ lib/db/seed.ts
@@ -0,0 +1,27 @@
+import data from "@/lib/data";
+import { connectToDatabase } from ".";
+import Product from "./models/product.model";
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+
+
+loadEnvConfig(cwd());
+
+const main = async () => {
+    try {
+        const {products} = data;
+        await connectToDatabase(process.env.MONGODB_URI);
+
+        await Product.deleteMany()
+        const createdProducts = await Product.insertMany(products)
+        console.log({
+            createdProducts,
+            message: "Products created successfully",
+        })
+        process.exit(0)
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to send database")
+    }
+}
+ main()
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formateNumberWithDecimals = (num: number): string => {
+  const [int,decimal] = num.toString().split(".")
+  return decimal ? `${int}.${decimal.slice(0,2)}` : int
+}
+//PROMT: [chatGTP] create toSlug ts arrow function that convert text to lowercase, remove non-word, non-whitespace, non-hyphen characters, replace whitespace, trim leading hypens and trim trailing hypens
+
+export const toSlug = (text: string): string => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "")
+    .replace(/\s+/g, "-")
+}
